bb57f451fe44364b07c8cd8159ffb3f4f9b5c958
DeweyTech <olivier.tavel@gmail.com>
2025-09-11 12:11:00 +0200

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -368,7 +368,7 @@
   // # maybe good for ARMCHAIR #
   #define FILTER                  3276    //  0.05f
   #define SPEED_COEFFICIENT       8192    //  0.5f  
-  #define STEER_COEFFICIENT       62259   // -0.2f
+  #define STEER_COEFFICIENT       20000 // AVANT : 62259   // -0.2f
   // #define SUPPORT_BUTTONS                 // Define for Nunchuk buttons support
 #endif
 // ############################# END OF VARIANT_NUNCHUK SETTINGS #########################
